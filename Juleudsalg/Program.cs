e4219f794a03a53da3bc873500d59b4b6286d6e7
Kirsten <Kigaa19@student.sdu.dk>
2024-09-13 13:54:38 +0200

    If LOOP

--- Juleudsalg/Program.cs
+++ Juleudsalg/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int i=21816000;
+double pris=599.95;
+bool jul;
+int Spm =60*60*24*30;
+jul =(i>=Spm*11) && (i<Spm*12);  
+Console.WriteLine("pris");
+if (jul){
+	double reduceretpris=600.0*0.7;
+	Console.WriteLine (reduceretpris);
+	}
+Console.WriteLine(pris);
