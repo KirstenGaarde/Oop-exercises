f994cc32d8a4239a2bbb48392628ebae97e5c624
Kirsten <Kigaa19@student.sdu.dk>
2024-09-18 13:24:23 +0200

    Solved problem thanks to Peter that amazing son of a bitch

---
+++ Array/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] months;
+months = [1,2,12];
+int arrayLength = months.Length;
+Console.WriteLine(arrayLength); 
+for (int i=0; i<months.Length; i++) 
+{
+	Console.WriteLine("Months" + (i+1)+" has "+months[i]+" days");
+}
