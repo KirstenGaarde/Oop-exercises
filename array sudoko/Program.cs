782f01324ee19f878fff45d39cbe3509357ffbeb
Kirsten <Kigaa19@student.sdu.dk>
2024-09-19 08:17:43 +0200

    good luck

---
+++ array sudoko/Program.cs
@@ -0,0 +1,8 @@
+﻿int[[][]]][[][][]][[][][]];
+int hus = hus.Length;
+Console.WriteLine(hus.Length); 
+for (int i=0; i<number.Length; i+) 
+{
+	Console.WriteLine("Number" + (i+1)+" has "+number[i]+);
+}
+
