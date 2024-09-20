09e0b7b72df7ecb675e53954493af11efd46ab62
Kirsten <Kigaa19@student.sdu.dk>
2024-09-20 12:45:35 +0200

    Array

---
+++ arrays/Program.cs
@@ -0,0 +1,9 @@
+﻿//Array temeperatur for loop
+
+double [] temperatur = {21.5,23.7,19.6,22.5,25.3,21.8,18.9};
+
+//arry loop
+for (int d=0; d<temperatur.Length-1; d ++)
+{
+	Console.WriteLine (temperatur [d+1]- temperatur[d]);
+}
