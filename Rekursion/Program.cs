1d18f3e172f1557f8845beb9c95ece2c8c2b8dd0
Kirsten <Kigaa19@student.sdu.dk>
2024-09-24 10:04:47 +0200

    Rekursion

--- Rekursion/Program.cs
+++ Rekursion/Program.cs
@@ -1,15 +1,9 @@
-﻿int factorial=(1*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18);
-int factorial(int n);
-int factorial (int n) {
-	if (n==0) {
-		return 0;
-} else if (n==1) {
-	return 1;
-} else {
-	return int factorial(int n);
-}
-}
-
-for (int n=0; n<18; n++) {
-	Console.WriteLine(n+ ": "+factorial(n));
-}
+﻿//Fakultet af N 
+/*skal have en basecase, så funktionen ved hvornår den skal stoppe så den ikke kører til stackoverflow
+ det diffineres med en If (==)*/
+long fakultet (long n) { 
+	if (n==1)
+		return n;
+	return n * fakultet(n-1); 
+};
+Console.WriteLine ("Fakultet er =" + fakultet (18));
