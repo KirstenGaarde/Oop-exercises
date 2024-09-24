d872b213f481b57dfe43ad94a10cdf5aec47d8e7
Kirsten <Kigaa19@student.sdu.dk>
2024-09-24 09:11:05 +0200

    Test

--- test/Program.cs
+++ test/Program.cs
@@ -92,3 +92,47 @@ bool kanKøbes;
 kanKøbes = samletbeløb >= husPris;
 
 Console.WriteLine ("kan huset købes?" + kanKøbes);
+
+
+//Loops eller kontrolstrukturer. 
+/*Udfør kun den her kode, hvis et udtryk er true. Hvis en kode er true, så udfør kpden. Hvis udtrykket ikke er korrekt så udfør ikke koden*/
+
+int indtægt = 10000, udgift=7000;
+if (indtægt > udgift)
+	Console.WriteLine ("der er overskud!");
+
+// {} skal på, hvis der er flere koder, som skal udføres
+
+//else kan sættes på, hvis vores if statment ikke er sandt
+int udbud;
+udbud = 500;
+if (udbud >0)
+Console.WriteLine("Vi har overskud");
+else
+Console.WriteLine ("Åh nej, go work");	//Der bliver derfor udskrevet "vi har overskud" fordi 500 er større end 0
+
+//ELSE IF.
+/*så hvis udbud er i nul så er den ikke negativ, men vi har heller ikke flere penge, derfor har vi sat et ekstra
+else if på. Det gør vi for at gøre den mere speicifik. else if sætningen laver en ny betingelse. Og i else if sætninger/statments kan kun én af sætningerne være true*/
+int dice;
+dice = 1;
+if (dice >3) {
+	if (dice ==7) 
+		Console.WriteLine("DEAD");
+	else
+Console.WriteLine ("HIT");
+}
+		else if (dice==0){
+	Console.WriteLine("HIDE");
+		}
+	else 
+	Console.WriteLine("RUN");
+//else fanger ALT i et if statment
+//Nestede if-sætninger
+/*Der kan laves en if-sætning (huset kan købes) så skriv kode. Else if, så specificere vi den med kode, og så kan der komme en ny kode. 
+Og i den kan der så være er endnu et if-statment (hvis lånet>0) så skrives koden "Du må gerne låne alligevel" 
+vi kan jo kun komme ind i else if sætningen, hvis det første if statment er false. og det næste else if, så er true, kommer vi ind i det treide if. */
+
+
+
+/* Hvis der er flere muligheder og vi ikke gider burge else if/if nestede. SÅ kan det gøres på en anden måde.*/
