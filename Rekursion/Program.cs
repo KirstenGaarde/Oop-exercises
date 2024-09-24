dc316217ec42d628ad96e883a3d90fcb4fa9a267
Kirsten <Kigaa19@student.sdu.dk>
2024-09-24 10:27:26 +0200

    Areal fuktion

--- Rekursion/Program.cs
+++ Rekursion/Program.cs
@@ -7,3 +7,24 @@ long fakultet (long n) {
 	return n * fakultet(n-1); 
 };
 Console.WriteLine ("Fakultet er =" + fakultet (18));
+
+
+//Cirkler i Tal 14.9 
+// areal
+
+double pi;
+pi = 3.147;
+double aeral (int a) {
+	if (a==0)
+		return a;
+	return pi * Math.Pow(a,2.0); //Pow er en funktioner, der opløfter a i anden, havde der stået a, 3 var det i trejide 
+}
+Console.WriteLine("Arealet er =" + aeral(3));
+
+//Omkreds
+double omkreds (int a) {
+	if (a==0)
+		return a; 
+	return pi * 2 * a;
+}
+Console.WriteLine ("Omkredsen er =" + omkreds(3));
