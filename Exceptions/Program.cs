8ee44bd87e1d002509ea9a4861bddfefebb51dc1
Kirsten <Kigaa19@student.sdu.dk>
2024-09-25 13:47:46 +0200

    try/catch

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,13 +1,15 @@
-﻿int iterationer = 10;
+﻿int iterationer = 10; 
 int [] array = {1,2,3,4,5};
 
-//increment
-for (int i=0; i < iterationer ; i++) {
+//increment 
+/* Det er fordi der står iterationer i vores paraentes, der gør at det bliver til en exception. Havde der stået array.Length, havde vi ikke fået en fejl og der
+havde ikke været en expecition som skal try og derefte catch*/
+for (int i=0; i < iterationer ; i++) { //Exception. Som bliver kastet 
 try{
 	Console.WriteLine(array[i]);
 	array[i]++;
 }
-catch{
+catch{ //Her bliver den grebet, og koden/programmet kører videre
 	Console.WriteLine ("Du har oversteget maksimum prøv igen");
 }
 }
